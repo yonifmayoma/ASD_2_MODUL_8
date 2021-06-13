c033fa10f41d773d3a383ce39504097a34df9dcb
Mayonice <Mayonice@LAPTOP-02UJB4GM>
2021-06-13 21:31:21 +0700

    TUGAS 8

---
+++ src/TUGAS_8/TreeNode.java
@@ -0,0 +1,10 @@
+package TUGAS_8;
+public class TreeNode {
+        char data;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode(int data){
+        this.data = (char) data;
+    }
+}
